44ca2cab6deeb071a9c7ac9aa9b8aebcb95af3b4
Steven Buglione <stevenbuglione1@gmail.com>
2026-08-09 10:55:04 -0400

    ci: pin recovered release authority

--- internal/qualitygate/main.go
+++ internal/qualitygate/main.go
@@ -33,7 +33,7 @@ const (
 	spiceTool             = toolchainModule + "/cmd/spice"
 	coreAnnotationTool    = toolchainModule + "/cmd/spice-annotation-core"
 	minimumCoverage       = 85.0
-	releaseWorkflowCommit = "c4df74b2c60640c60fe0fa3fe641dadafbc4148a"
+	releaseWorkflowCommit = "07f898b85e7d1c409b91bf280e47d62921e786b6"
 )
 
 var output io.Writer = os.Stdout
